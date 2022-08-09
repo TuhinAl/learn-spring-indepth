b7b62c2c158a01d739c02f1666169da8599ddd4d
Alauddin Tuhin <alauddintuhin1411@gmail.com>
2022-08-09 17:47:36 +0600

    git test

---
+++ src/main/java/com/altuhin/spring/TestClassForGitConfig.java
@@ -0,0 +1,10 @@
+package com.altuhin.spring;
+
+public class TestClassForGitConfig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Multiple git from same env");
+	}
+
+}
